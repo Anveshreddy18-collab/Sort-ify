84721925dade13f0725eba7b0a3d9530329bb7c1
Jack Bisceglia <jackbisceglia2000@gmail.com>
2020-04-02 00:51:24 -0400

    Canvas added, fractal functions defined

---
+++ app.js
@@ -0,0 +1,20 @@
+function kochCurve(p1, p2, depth){
+    // base case if depth = 0 -> draw line from p1 to p2
+    // recursive case - find the rest of the points and make recursive calls w/ depth - 1
+}
+
+function tree(p1, p2, depth){
+    // base case if depth = 0 -> draw line from p1 to p2
+    // draw(from p1 to p3)
+    // recursive case - find the rest of the points and make recursive calls w/ depth - 1
+}
+
+function sierpTriangle(p1, p2, p3, depth){
+    // base case if depth = 0 -> draw triangle with p1, p2, p3
+    // recursive case - get 3 mid points and make recursive calls w/ depth - 1
+}
+
+function sierpCarpet(p1, length, depth){
+    // base case if depth = 0 -> draw rectangle with p1 and l/w = length
+    // recursive case - find bottom left of all 8 smaller rects and make recursive calls on each w/ depth -1 
+}
