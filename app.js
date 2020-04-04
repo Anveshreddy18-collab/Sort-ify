bb962a035372835405859cb15804e942058738c7
Jack Bisceglia <jackbisceglia2000@gmail.com>
2020-04-04 19:54:25 -0400

    Styling Updated, and Array size selection and generation added

--- app.js
+++ app.js
@@ -1,76 +1,49 @@
-canvas = document.getElementById('canvas1');
-ctx = canvas.getContext('2d');
+function draw(array) {
+  document.querySelectorAll(".bar").forEach((bar) => bar.remove());
 
-function kochCurve(p1, p2, depth){
-    // base case if depth = 0 -> draw line from p1 to p2
-    // recursive case - find the rest of the points and make recursive calls w/ depth - 1
-    if (depth == 0){
-        // draw line
-        drawLine(p1, p2);
-        return
-    }
-    else{
-        let [x1, y1] = p1, [x2, y2] = p2;
-
-        let distance = x2 - x1;
-        let distToMove = distance/3;
-        
-        //move turtle first increment
-        let p3 = [x1 + distToMove, y1];
-        ctx.moveTo(p3[0], p3[1]);
-    
-        //second increment
-        // ctx.rotate(60);
-        let p4 = [x1 + (distToMove * 2), y1 - 200];
-        
-        //third increment
-        // ctx.rotate(-120);
-        let p5 = [x1 + (distToMove * 3), y1];
-        
-        //recursive calls
-        kochCurve(p1, p3, depth - 1);
-        kochCurve(p3, p4, depth - 1);
-        kochCurve(p4, p5, depth - 1);
-        kochCurve(p5, p2, depth - 1);
-
-        
-    }
+  let container = document.querySelector(".inner");
+  array.forEach((value) => {
+    let bar = container.appendChild(document.createElement("div"));
+    let val = bar.appendChild(document.createElement("h1"));
+    val.textContent = value;
+    val.className = "arr-val";
+    bar.className = "bar";
+    bar.style.height = 20 + value * 10 + "px";
+    bar.style.width = Math.round(200 / array.length) + "px";
+  });
 }
 
-function tree(p1, p2, depth){
-    // base case if depth = 0 -> draw line from p1 to p2
-    // draw(from p1 to p3)
-    // recursive case - find the rest of the points and make recursive calls w/ depth - 1
-    if (depth == 0){
-        // draw line
+function generateArray(numEls) {
+  let newArr = [];
+  for(let i = 0; i < numEls; i++){
+    let foundUnique = false;
+    while(foundUnique != true){
+      let randNum = Math.round(Math.random() * 25) + 5;
+      if(newArr.includes(randNum) != true){
+        newArr.push(randNum);
+        foundUnique = true;
+      }
     }
-}
-
-function sierpTriangle(p1, p2, p3, depth){
-    // base case if depth = 0 -> draw triangle with p1, p2, p3
-    // recursive case - get 3 mid points and make recursive calls w/ depth - 1
-    if (depth == 0){
-        // draw triangle
+  }
+  let sorted = true;
+  for(let i = 0; i < numEls - 1; i++){
+    if(newArr[i] > newArr[i+1]){
+      sorted = false;
+      break;
     }
+  }
+
+  if(sorted === true){
+    newArr = [];
+    generateArray(numEls);
+  }
+
+  return newArr;
 }
 
-function sierpCarpet(p1, length, depth){
-    // base case if depth = 0 -> draw rectangle with p1 and l/w = length
-    // recursive case - find bottom left of all 8 smaller rects and make recursive calls on each w/ depth -1 
-    if (depth == 0){
-        // draw rectangle/square
-    }
-}
+let dropDown = document.querySelector('#choose');
 
-function drawLine(p1, p2){
-    let [x1, y1] = p1;
-    let [x2, y2] = p2;
-    ctx.moveTo(x1,y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-
-}
-
-window.addEventListener('load', () => {
-    kochCurve([100, 400], [900, 400], 1);  
-})
+draw(generateArray(dropDown.value));
+dropDown.addEventListener('change', () =>{
+  draw(generateArray(dropDown.value));
+});
