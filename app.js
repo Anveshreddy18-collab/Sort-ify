36a3a27b793e3c605c1ed020cd082a592b48bc84
Jack Bisceglia <jackbisceglia2000@gmail.com>
2020-04-15 17:34:51 -0400

    Merge Sort functionality added, interactive algorithm titles, buttons invoke correct algorithm

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 let btn = document.querySelector("#viz");
 let stopBtn = document.querySelector('#stop');
 
+// Algorithm Title
+let algTitle = document.querySelector('#sortType');
+
 // ALGORITHM BUTTONS
-let bubbleButton = document.getElementById('bubbleButton');
-let mergeButton = document.getElementById('mergeButton');
-let quickButton = document.getElementById('quickButton');
+let bubbleButton = document.getElementById('bubbleBtn');
+let mergeButton = document.getElementById('mergeBtn');
+let quickButton = document.getElementById('quickBtn');
 
 // MAIN GRAPH DIV
 let graph = document.querySelector(".inner");
@@ -13,10 +16,29 @@ let graph = document.querySelector(".inner");
 // Global Stop Variable
 let stop = false;
 
+// Global Active Alg Object
+let activeAlg = {
+  'bubble' : false,
+  'merge' : false,
+  'quick' : false
+};
+
+// Initialize Active Algorithm to bubble
+activateSort('bubble');
+algTitle.innerHTML = 'Bubble Sort'
+
+
+// Make Each Alg button change the state of the active Alg object
+mergeButton.addEventListener('click', () => {activateSort('merge'); algTitle.innerHTML = mergeButton.textContent;})
+quickButton.addEventListener('click', () => {activateSort('quick'); algTitle.innerHTML = quickButton.textContent;})
+bubbleButton.addEventListener('click', () => {activateSort('bubble'); algTitle.innerHTML = bubbleButton.textContent;})
+
+// STOP VISUALIZATION
 stopBtn.addEventListener('click', () => {
   stop = true;
 })
 
+// START VISUALIZATION
 btn.addEventListener("click", () => {
   stop = false;
   function drawLoop() {
@@ -33,20 +55,50 @@ btn.addEventListener("click", () => {
 
   let numEls = document.querySelector("#choose").value;
 
-  let delayTimes = {
+  let bubbleDelayTimes = {
     '30' : 25,
     '20' : 60,
     '15' : 75
   };
 
-  let delay = delayTimes[`${numEls}`];
-  console.log(delay); // ms (sped up for demo)
+  let mergeDelayTimes = {
+    '30' : 220,
+    '20' : 250,
+    '15' : 300
+  };
+
+  // let delay = delayTimes[`${numEls}`];  
   let array = generateArray();
-  let iterator = bubbleSort(array);
+
+  let delay;
+  let iterator;
+
+  if (activeAlg['bubble']){
+    delay = bubbleDelayTimes[`${numEls}`];
+    iterator = bubbleSort(array);
+  }
+  else if (activeAlg['merge']){
+    delay = mergeDelayTimes[`${numEls}`];
+    iterator = mergeSort(array);
+  }
+  else{
+    delay = bubbleDelayTimes[`${numEls}`];
+    iterator = bubbleSort(array);
+  }
 
   let intervalId = setInterval(drawLoop, delay);
   drawLoop(); // so no wait for first paint
 });
+
+// Function to change the global active algorithm object
+function activateSort(alg){
+  activeAlg = {
+    'bubble' : false,
+    'merge' : false,
+    'quick' : false
+  };
+  activeAlg[alg] = 'true';
+}
 
 // --- DRAW FUNCTION ---
 function draw(array) {
@@ -99,7 +151,7 @@ function isSorted(newArr) {
   return sorted;
 }
 
-// --- BUBBLE SORT GENERATOR FUNCTION ---
+// --- BUBBLE SORT ---
 function* bubbleSort(array) {
   for (let i = 0; i < array.length; i++) {
     for (let j = 0; j < array.length; j++) {
@@ -112,6 +164,50 @@ function* bubbleSort(array) {
       if (stop == true){
         return;
       }
+    }
+  }
+}
+
+// --- MERGE SORT ---
+// Merge Sort Helper
+function merge(A, temp, start, middle, last)
+{
+  let k = start, i = start, j = middle + 1;
+
+  while (i <= middle && j <= last) {
+    if (A[i] < A[j]) {
+      temp[k++] = A[i++];
+    } else {
+      temp[k++] = A[j++];
+    }
+  }
+
+  while (i < A.length && i <= middle) {
+    temp[k++] = A[i++];
+  }
+
+  for (i = start; i <= last; i++) {
+    A[i] = temp[i];
+  }
+}
+
+// Merge Sort Generator
+function* mergeSort(A)
+{
+  let low = 0, high = A.length - 1;
+
+  let temp = A.slice(A, A.length);
+
+  for (m = 1; m <= high - low; m = 2*m){
+    for (i = low; i < high; i += 2*m){
+      let start = i, mid = i + m - 1, last = Math.min(i + 2 * m - 1, high);
+      
+      merge(A, temp, start, mid, last);
+      yield A;
+    }
+    
+    if (stop == true){
+      return;
     }
   }
 }
