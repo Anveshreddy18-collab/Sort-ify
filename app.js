e36fea03528d0d79dbc503a7a4c5b42dde8470ba
Jack Bisceglia <jackbisceglia2000@gmail.com>
2020-04-16 22:57:21 -0400

    Iterative Quick Sort algorithm implemented. Bug when trying to call function

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // VIZ AND STOP BUTTONS
 let btn = document.querySelector("#viz");
-let stopBtn = document.querySelector('#stop');
+let stopBtn = document.querySelector("#stop");
 
 // Algorithm Title
-let algTitle = document.querySelector('#sortType');
+let algTitle = document.querySelector("#sortType");
 
 // ALGORITHM BUTTONS
-let bubbleButton = document.getElementById('bubbleBtn');
-let mergeButton = document.getElementById('mergeBtn');
-let quickButton = document.getElementById('quickBtn');
+let bubbleButton = document.getElementById("bubbleBtn");
+let mergeButton = document.getElementById("mergeBtn");
+let quickButton = document.getElementById("quickBtn");
 
 // MAIN GRAPH DIV
 let graph = document.querySelector(".inner");
@@ -18,39 +18,39 @@ let stop = false;
 
 // Global Active Alg Object
 let activeAlg = {
-  'bubble' : false,
-  'merge' : false,
-  'quick' : false
+  bubble: false,
+  merge: false,
+  quick: false,
 };
 
 // Initialize Active Algorithm to bubble
-activateSort('bubble');
-algTitle.innerHTML = 'Bubble Sort'
-bubbleButton.style.boxShadow = '0px 0px 20px 1px rgb(255, 96, 184, .8)';
+activateSort("bubble");
+algTitle.innerHTML = "Bubble Sort";
+bubbleButton.style.boxShadow = "0px 0px 20px 1px rgb(255, 96, 184, .8)";
 
 // Make Each Alg button change the state of the active Alg object
-bubbleButton.addEventListener('click', () => {
-  activateSort('bubble');
+bubbleButton.addEventListener("click", () => {
+  activateSort("bubble");
   algTitle.innerHTML = bubbleButton.textContent;
   changeColor(bubbleButton, mergeButton, quickButton);
-})
+});
 
-mergeButton.addEventListener('click', () => {
-  activateSort('merge');
+mergeButton.addEventListener("click", () => {
+  activateSort("merge");
   algTitle.innerHTML = mergeButton.textContent;
   changeColor(mergeButton, bubbleButton, quickButton);
-})
+});
 
-quickButton.addEventListener('click', () => {
-  activateSort('quick');
+quickButton.addEventListener("click", () => {
+  activateSort("quick");
   algTitle.innerHTML = quickButton.textContent;
   changeColor(quickButton, mergeButton, bubbleButton);
-})
+});
 
 // STOP VISUALIZATION
-stopBtn.addEventListener('click', () => {
+stopBtn.addEventListener("click", () => {
   stop = true;
-})
+});
 
 // START VISUALIZATION
 btn.addEventListener("click", () => {
@@ -70,34 +70,32 @@ btn.addEventListener("click", () => {
   let numEls = document.querySelector("#choose").value;
 
   let bubbleDelayTimes = {
-    '30' : 25,
-    '20' : 60,
-    '15' : 75
+    "30": 25,
+    "20": 60,
+    "15": 75,
   };
 
   let mergeDelayTimes = {
-    '30' : 220,
-    '20' : 250,
-    '15' : 300
+    "30": 220,
+    "20": 250,
+    "15": 300,
   };
 
-  // let delay = delayTimes[`${numEls}`];  
+  // let delay = delayTimes[`${numEls}`];
   let array = generateArray();
 
   let delay;
   let iterator;
 
-  if (activeAlg['bubble']){
+  if (activeAlg["bubble"]) {
     delay = bubbleDelayTimes[`${numEls}`];
     iterator = bubbleSort(array);
-  }
-  else if (activeAlg['merge']){
+  } else if (activeAlg["merge"]) {
     delay = mergeDelayTimes[`${numEls}`];
     iterator = mergeSort(array);
-  }
-  else{
+  } else if (activeAlg["quick"]) {
     delay = bubbleDelayTimes[`${numEls}`];
-    iterator = bubbleSort(array);
+    iterator = quickSort(array);
   }
 
   let intervalId = setInterval(drawLoop, delay);
@@ -105,20 +103,20 @@ btn.addEventListener("click", () => {
 });
 
 // Function to change the global active algorithm object
-function activateSort(alg){
+function activateSort(alg) {
   activeAlg = {
-    'bubble' : false,
-    'merge' : false,
-    'quick' : false
+    bubble: false,
+    merge: false,
+    quick: false,
   };
-  activeAlg[alg] = 'true';
+  activeAlg[alg] = "true";
 }
 
 // Function to change button colors when picked
-function changeColor(btn1, btn2, btn3){
-  btn1.style.boxShadow = '0px 0px 20px 1px rgb(255, 96, 184, .8)';
-  btn2.style.boxShadow = '';
-  btn3.style.boxShadow = '';
+function changeColor(btn1, btn2, btn3) {
+  btn1.style.boxShadow = "0px 0px 20px 1px rgb(255, 96, 184, .8)";
+  btn2.style.boxShadow = "";
+  btn3.style.boxShadow = "";
 }
 
 // --- DRAW FUNCTION ---
@@ -182,7 +180,7 @@ function* bubbleSort(array) {
         array[j + 1] = temp;
       }
       yield array;
-      if (stop == true){
+      if (stop == true) {
         return;
       }
     }
@@ -191,9 +189,10 @@ function* bubbleSort(array) {
 
 // --- MERGE SORT ---
 // Merge Sort Helper
-function merge(A, temp, start, middle, last)
-{
-  let k = start, i = start, j = middle + 1;
+function merge(A, temp, start, middle, last) {
+  let k = start,
+    i = start,
+    j = middle + 1;
 
   while (i <= middle && j <= last) {
     if (A[i] < A[j]) {
@@ -213,22 +212,75 @@ function merge(A, temp, start, middle, last)
 }
 
 // Merge Sort Generator
-function* mergeSort(A)
-{
-  let low = 0, high = A.length - 1;
+function* mergeSort(A) {
+  let low = 0,
+    high = A.length - 1;
 
   let temp = A.slice(A, A.length);
 
-  for (m = 1; m <= high - low; m = 2*m){
-    for (i = low; i < high; i += 2*m){
-      let start = i, mid = i + m - 1, last = Math.min(i + 2 * m - 1, high);
-      
+  for (m = 1; m <= high - low; m = 2 * m) {
+    for (i = low; i < high; i += 2 * m) {
+      let start = i,
+        mid = i + m - 1,
+        last = Math.min(i + 2 * m - 1, high);
+
       merge(A, temp, start, mid, last);
       yield A;
     }
-    
-    if (stop == true){
+
+    if (stop == true) {
       return;
     }
   }
 }
+
+// --- QUICK SORT ---
+function swap(array, a, b){
+  let temp = array[a]; 
+  array[a] = array[b]; 
+  array[b] = temp; 
+  return array;
+}
+
+function partition(array, a, b){
+  let x = array[b];
+  let i = (a - 1);
+
+  for(j = a; j <= b; j++){
+    if(array[j] <= x){
+      i += 1;
+      array = swap(array, i, j);
+    }
+  }
+  array = swap(array, i + 1, b);
+  return (i + 1);
+}
+
+function quickSort(array, a, b){
+  let stack = new Array();
+
+  stack.push(a);
+  stack.push(b);
+
+  while(stack.length >= 0){
+    b = stack.pop();
+    a = stack.pop();
+
+    let p = partition(array, a, b);
+
+    if(p - 1 > a){
+      stack.push(a);
+      stack.push(p - 1)
+    }
+
+    if(p + 1 < b){
+      stack.push(p + 1);
+      stack.push(b);
+    }
+  }  
+  return array;
+}
+
+
+arr = [1, 5, 4, 2];
+quickSort(arr, 0, arr.length - 1);
